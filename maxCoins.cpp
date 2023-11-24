6aa96aebf3a50902907b2f02b6cb47994fc07102
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2023-11-24 12:21:33 +0530

    maxCoins

---
+++ maxCoins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        int ans=0;
+        int n = piles.size();
+        for(int i=n/3; i<n; i+=2)
+          ans+=piles[i];
+        return ans; 
+    }
+};
