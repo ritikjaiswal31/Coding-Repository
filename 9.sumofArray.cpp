5c0d367c029c4b5017c55faa245af7d201276f46
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2024-02-07 13:57:04 +0530

    sumofArray

---
+++ 9.sumofArray.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int sum(int arr[], int n){
+    int sum = 0;
+    for(int i=0; i<n; i++){
+        sum+=arr[i];
+    }
+    return sum;
+}
+
+int main(){
+    
+    int arr[] = {1,2,4,5,6,7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout<<"Sum: " << sum(arr,n);
+    
+    return 0;
+}
