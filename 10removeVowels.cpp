c355c3be60b232a1156c6b4662add223b97c088a
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2024-02-19 21:24:04 +0530

    removeVowels

--- 10removeVowels.cpp
+++ 10removeVowels.cpp
@@ -4,15 +4,19 @@ using namespace std;
 
 int main(){
 
-    string str = "ritikjaiswal";
-    int vowels=0;
-    for(int i=0; str[i]!='\0'; i++){
-        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U'){
-            vowels++;
-
-        }
+    int j=0;
+    string str="RitikJaiswal";
+    for(int i=0; i<str[i]!='\0'; i++){
+        if(str[i]!='a' && str[i]!='e' && str[i]!='i' && str[i] !='o' && str[i]!='A' && str[i]!='E' && str[i]!='I' && str[i]!='O' && str[i]!='U'){
+            str[j++] = str[i];
+           }
     }
-    cout << "Number of vowels in the string: " << vowels << endl;
+
+    while(j<str.size()){
+        str[j]='\0';
+        j++;
+    }
+    cout << "String without vowels: " << str << endl;
 
     return 0;
 }
