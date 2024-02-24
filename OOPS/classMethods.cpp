fdf06a4c02382b12658218a5ef4a369718afdf71
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2024-02-24 15:50:07 +0530

    classMethods

---
+++ OOPS/classMethods.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class MyClass {
+    public:
+      void myMethod() {
+        cout << "Hello World";
+      }
+};
+
+int main(){
+
+    MyClass myObj;
+    myObj.myMethod();
+    return 0;
+}
