39bf2739e9c3cf3a170bab950820db9b347d6f35
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2024-03-18 16:12:00 +0530

    bubbleSort

--- Sorting Algorithms/bubbleSort.cpp
+++ Sorting Algorithms/bubbleSort.cpp
@@ -3,26 +3,31 @@ using namespace std;
 
 void bubbleSort(int arr[], int n){
     int i, j;
-    bool swapped;
-    for(i=0; i<n-1; i++){
-        swapped=false;
-        for(j=0; j<n-1; j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j], arr[j+1]);
-                swapped = true;
-            }
-        }
-        if(swapped == false)
-          break;
-    }
+    for(int i=0; i<n-1; i++)
+        for(int j=0;j<n-i-1; j++)
+          if(arr[j] > arr[j+1])
+             swap(arr[j], arr[j+1]);
+    
+    
+    
+}
+
+void printArray(int arr[], int size){
+        int i;
+        for(int i=0; i<size; i++)
+           cout << arr[i], " ";
+        cout << endl;
+
 }
 
 int main(){
 
-    int arr[] = {1,9,90,82,47};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n);
-    cout << "Sorted Array: \n";
-    printArray(arr, n);
+    int arr[] = {5,1,6,2,8};
+    int N = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, N);
+    cout << "BubbleSort is: ";
+    printArray(arr, N);
+
+
     return 0;
 }
