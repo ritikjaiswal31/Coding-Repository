507b4712aa89f7648bcf0e07f4c904d26d79d408
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2024-03-26 20:13:59 +0530

    Placement

---
+++ Placement/binarySeach.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int left, int right, int x){
+    while(left < right){
+        int mid = left + (rigght - left) / 2;
+        if(arr[mid] == x)
+           return m;
+    }
+}
+
+int main(){
+
+
+
+    return 0;
+}
---
+++ Placement/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+unsigned int factorial(unsigned int n){
+    if(n==0 || n==1)
+        return 1;
+    return n*factorial(n-1);
+}
+
+int main(){
+
+    int num=5;
+    cout
+
+    return 0;
+}
