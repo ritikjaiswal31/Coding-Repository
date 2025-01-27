815dce110b23ef2f0cb1cf247bbe9ac623930b7d
ritikjaiswal31 <ritikjaiswal2002@gmail.com>
2025-01-27 15:24:11 +0530

    Updated ReadMe

--- LeetCode POTD C++/isReachableAtTime.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        int x = abs(sx - fx), y = abs(sy - fy);
-        if(x == 0 && y == 0 && t == 1) return false;
-        return max(x, y) <= t;
-    }
-};
